4f2e510d754078afa497affa2e3faaa7ef4165ad
NIIOTAS <niiotasalokome@gmail.com>
2026-07-01 20:56:21 +0000

    question2 - solved

--- Algo.js
+++ Algo.js
@@ -1,5 +1,12 @@
 // print numbers 1 to 10.
 
-for(var i = 1; i>= 10; i++){
+for(var i = 1; i<= 10; i++){
     console.log(i);
-}
+};
+
+// print odd numbers from 1 to 100.
+
+for(var i = 1; i<= 100; i+=2){
+    console.log(i);
+};
+
