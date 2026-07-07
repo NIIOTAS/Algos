add937e009a2cb2c62c35511b2df18751c20cd8e
NIIOTAS <niiotasalokome@gmail.com>
2026-07-07 19:34:08 +0000

    7th_July

---
+++ Algo7.js
@@ -0,0 +1,8 @@
+//calculate the average of numbers in an array.
+function calculateAverage(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum / numbers.length;
+}
