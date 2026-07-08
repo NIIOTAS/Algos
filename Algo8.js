fc1b14cf39fc230aa4038b0c9e36b9804391cb43
NIIOTAS <niiotasalokome@gmail.com>
2026-07-08 15:34:44 +0000

    9th -july

--- Algo8.js
+++ Algo8.js
@@ -1,1 +1,10 @@
- 
+ //create a function that receives an array of numbers as argument and returns an array containing only the positive numbers.
+function getPositiveNumbers(numbers) {
+    let positiveNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+            positiveNumbers.push(numbers[i]);
+        }
+    }
+    return positiveNumbers;
+}
